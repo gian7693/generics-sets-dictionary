ceb77278606c8a73bbcfe0c1fe257ddfa1c90ff9
Gian Makoto <gianmakoto@hotmail.com>
2024-01-24 13:34:28 -0300

    Generics PARTE 1 / 2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Course.Models;
+
+namespace Course // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<String> printService = new PrintService<String>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                printService.AddValue(value);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+            
+        }
+    }
+}
