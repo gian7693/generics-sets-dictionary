dc4584b4cf227b3930264b45c77d1e421735fbfd
Gian Makoto <gianmakoto@hotmail.com>
2024-01-25 14:38:30 -0300

    Dictionary e SortedDictionary

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Security.Cryptography.X509Certificates;
-using Course.Entities;
 
 namespace Course // Note: actual namespace depends on the project name.
 {
@@ -10,49 +7,33 @@ namespace Course // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            HashSet<Student> turmaA = new HashSet<Student>();
-            HashSet<Student> turmaB = new HashSet<Student>();
-            HashSet<Student> turmaC = new HashSet<Student>();
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
 
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "(16) 99712234";
+            cookies["phone"] = "(15) 83737388";
 
-            Console.Write("How many students for course A? ");
-            int nA = int.Parse(Console.ReadLine());
-            for (int i = 0; i < nA; i++)
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
             {
-                int cod = int.Parse(Console.ReadLine());
-                Student aluno = new Student(cod);
-                turmaA.Add(aluno);
+                Console.WriteLine(cookies["email"]);
             }
-            Console.Write("How many students for course B? ");
-            int nB = int.Parse(Console.ReadLine());
-            for (int i = 0; i < nB; i++)
+            else
             {
-                int cod = int.Parse(Console.ReadLine());
-                Student aluno = new Student(cod);
-                turmaB.Add(aluno);
-            }
-            Console.Write("How many students for course C? ");
-            int nC = int.Parse(Console.ReadLine());
-            for (int i = 0; i < nC; i++)
-            {
-                int cod = int.Parse(Console.ReadLine());
-                Student aluno = new Student(cod);
-                turmaC.Add(aluno);
+                Console.WriteLine("There's no 'email' key");
             }
 
-            HashSet<Student> total = new HashSet<Student>(turmaA);
-            total.UnionWith(turmaB);
-            total.UnionWith(turmaC);
+            Console.WriteLine("Size: " + cookies.Count);
 
-            Console.WriteLine($"Total students: {total.Count}");
-            PrintCollection(total);
-        }
-
-        static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach (T obj in collection)
+            Console.WriteLine("ALL COOKIES: ");
+            foreach(KeyValuePair<string, string> item in cookies)
             {
-                Console.Write(obj + " ");
+                Console.WriteLine(item.Key + ": " + item.Value);
             }
         }
     }
