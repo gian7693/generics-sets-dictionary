8881fe04a8ff46f8f7440a2d2d6e55384d37c62e
Gian Makoto <gianmakoto@hotmail.com>
2024-01-25 11:01:22 -0300

    Como coleções hash comparam igualdade

--- Program.cs
+++ Program.cs
@@ -8,51 +8,19 @@ namespace Course // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            #region Conjuntos: HashSet e SortedSet PARTE 1
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.00));
+            a.Add(new Product("Notebook", 1200.00));
 
-            //HashSet<String> set = new HashSet<string>();
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
 
-            //set.Add("TV");
-            //set.Add("Notebook");
-            //set.Add("Tablet");
+            Product prod = new Product("Notebook", 1200.00);
+            Console.WriteLine(a.Contains(prod));
 
-            //Console.WriteLine(set.Contains("Notebook"));
-
-            //foreach( string p in set )
-            //{
-            //    Console.WriteLine(p);
-            //}
-
-            #endregion
-
-            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
-            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
-
-            // UNION
-            SortedSet<int> c = new SortedSet<int>(a);
-            c.UnionWith(b);
-            PrintCollection(c);
-
-            // INTERSECTION
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(b);
-            PrintCollection(d);
-
-            // DIFFERENCE
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(b);
-            PrintCollection(e);
-
-        }
-
-        static void PrintCollection<T>(IEnumerable<T> collection)
-        {
-            foreach(T obj in collection)
-            {
-                Console.Write(obj + " ");
-            }
-
-            Console.WriteLine();
+            Point point = new Point(3, 4);
+            Console.WriteLine(b.Contains(point));
         }
     }
 }
