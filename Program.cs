ac8cd82b029df1c3963c518942fae9bb01dd0deb
Gian Makoto <gianmakoto@hotmail.com>
2024-01-25 09:50:36 -0300

    Conjuntos: HashSet e SortedSet PARTE 1 / 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System; 
+﻿using System;
+using System.Collections.Generic;
 using Course.Entities;
 
 namespace Course // Note: actual namespace depends on the project name.
@@ -7,12 +8,51 @@ namespace Course // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
-            Client b = new Client() { Name = "Alex", Email = "maria@gmail.com" };
+            #region Conjuntos: HashSet e SortedSet PARTE 1
 
-            Console.WriteLine(a.Equals(b));
-            Console.WriteLine(a.GetHashCode());
-            Console.WriteLine(b.GetHashCode());
+            //HashSet<String> set = new HashSet<string>();
+
+            //set.Add("TV");
+            //set.Add("Notebook");
+            //set.Add("Tablet");
+
+            //Console.WriteLine(set.Contains("Notebook"));
+
+            //foreach( string p in set )
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            #endregion
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            // UNION
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            PrintCollection(c);
+
+            // INTERSECTION
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            PrintCollection(d);
+
+            // DIFFERENCE
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach(T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
